95a6219c2598af75fbfc0213397b528d0c6b63c8
IvanGodinez21 <ivano.godinez@gmail.com>
2022-08-14 15:28:43 -0500

    Initial commit

---
+++ src/clients/discordApi.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import DiscordOAuth2 from 'discord-oauth2';
+
+export default new DiscordOAuth2({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+});
---
+++ src/routes/main.routes.js
@@ -0,0 +1,49 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Router } from 'express';
+import crypto from 'crypto';
+import discordApi from '../clients/discordApi.js';
+
+const scopes = process.env.CLIENT_SCOPES.split(',');
+
+export const router = Router();
+
+router.get('/', (req, res) => {
+    res.send('If you want to learn more about this app, please visit: https://github.com/ClunkyTeam/Discord-OAuth2-Server');
+});
+
+router.get('/login', (req, res) => {
+    let url = (discordApi.generateAuthUrl({
+        scope: scopes,
+        state: crypto.randomBytes(16).toString('hex'),
+    }));
+    res.redirect(url);
+});
+
+router.get('/callback', async (req, res) => {
+    let credentials = await discordApi.tokenRequest({
+        code: req.query.code,
+        scope: scopes,
+        grantType: 'authorization_code',
+    });
+    credentials.expiration = new Date(Date.now() + (credentials.expires_in * 1000));
+    res.redirect(`${process.env.CLIENT_REDIRECT_URL}?access_token=${credentials.access_token}&refresh_token=${credentials.refresh_token}&expires_in=${credentials.expires_in}&scope=${credentials.scope}&token_type=${credentials.token_type}&expiration=${credentials.expiration}`);
+});
+
+router.post('/refresh_token', async (req, res) => {
+    try {
+        let credentials = await discordApi.tokenRequest({
+            refreshToken: req.body.refresh_token,
+            grantType: 'refresh_token',
+            scope: scopes
+        });
+        credentials.expiration = new Date(Date.now() + (credentials.expires_in * 1000));
+        res.send(credentials);
+    } catch (err) {
+        res.statusStatus(err.code).send(err.code);
+    }
+});
+
+router.get('*', (req, res) => {
+    res.redirect('/');
+});
